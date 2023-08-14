73b7cbaa5b065639078af8730c03455db4ea6b90
alexhilty <alex.hilty24@gmail.com>
2023-08-14 10:17:14 -0400

    added circuit collection template to ensure backwards compatibility with new circuit collection classes

--- CircuitCollection.py
+++ CircuitCollection.py
@@ -10,14 +10,108 @@ import numpy as np
 import operator as op
 import time
 import copy
+import pickle
 
 def unique_elem_it(iterator):
     '''Returns a list of unique elements from an iterator (preserving order)'''
 
     yield from dict.fromkeys(iterator)
+
+##### CIRCUIT COLLECTION TEMPLATE #####
+# defines the template for circuit collections
+class CircuitCollectionTemplate:
+
+    def __init__(self):
+        self.q_transpiled = []
+        self.q_circuits = []
+        self.circuits = []
+        self.images = []
+
+        self.built_circuits = False
+    
+    # Takes a set of gates and returns the corresponding index in circuits/q_circuits/q_transpiled
+    def gates_to_index(self, gates = []):
+        raise NotImplementedError
+
+    # returns the indecies of all direct children for a given index
+    def child_indecies(self, n1, n2):
+        raise NotImplementedError
+    
+    # converts given circuit to image based representation
+    def convert_to_image(self, *args):
+        n1, n2 = args[0][0], args[0][1]
+        raise NotImplementedError
+
+    # transpile circuits with n processes and transpile_args
+    # trials specifies how many times to try transpile before choosing the best result (based on circuit depth)
+    def transpile_circuits(self, n, trials, **transpile_args):
+        trans_start_time = time.time()
+        if not self.built_circuits:
+            print("transpile_circuits: No Circuits Built!")
+            return
+        
+        self.q_transpiled = []
+
+        transpiled_temp = []
+
+        # compute first set of transpiled circuits
+        for i in range(len(self.q_circuits)):
+            self.q_transpiled.append(
+                parallel_map(transpile, self.q_circuits[i], num_processes = n, task_kwargs = transpile_args)
+            )
+
+        print("--- Transpile (Trial " + str(1) + "): %s seconds ---" % (time.time() - trans_start_time))
+
+
+        # compute remaining trials and compare them to the first set, replacing them if they are better
+        for i in range(trials - 1):
+            for j in range(len(self.q_circuits)):
+
+                # Transpile circuits
+                transpiled_temp.append(
+                    parallel_map(transpile, self.q_circuits[j], num_processes = n, task_kwargs = transpile_args)
+                )
+
+                # Compare circuits
+                for k in range(len(self.q_circuits[j])):
+                    if (transpiled_temp[j][k].depth() < self.q_transpiled[j][k].depth()):
+                        self.q_transpiled[j][k] = transpiled_temp[j][k]
+
+             # print run time
+            print("--- Transpile (Trial " + str(i + 2) + "): %s seconds ---" % (time.time() - trans_start_time))
+            transpiled_temp = []
+
+        # print run time
+        print("--- Transpile (" + str(trials) + "): %s seconds ---" % (time.time() - trans_start_time))
+
+    # convert all circuits to image based representation
+    def convert_to_images(self):
+        # time how long it takes to convert all circuits
+        convert_start_time = time.time()
+
+        if not self.built_circuits:
+            print("convert_to_images: No Circuits Built!")
+            return
+
+        # FIXME: why did I think this was good ? lol
+        for i in range(len(self.circuits)):
+            # get index of all circuits
+            indecies = map(self.gates_to_index, self.circuits[i])
+
+            self.images.append(list(map(self.convert_to_image, indecies)))
+
+        # print run time
+        print("--- Convert: %s seconds ---" % (time.time() - convert_start_time))
+
+    def num_circuits(self):
+        sum = 0
+        for i in range(len(self.circuits)):
+            sum += len(self.circuits[i])
+        
+        return sum
     
 # defines collections of all derivative circuits from seed circuit
-class CircuitCollection:
+class CircuitCollection(CircuitCollectionTemplate):
     
     def __init__(self, seed = None, num_qubits = 0, depth = None, reps = None):
         self.seed = seed # seed gate list
@@ -50,48 +144,6 @@ class CircuitCollection:
     
     # Takes a set of gates and returns the corresponding index in circuits/q_circuits/q_transpiled
     def gates_to_index(self, gates = []):
-        # n1 = int(round(len(gates) - (sum(self.reps) - self.depth) - 1))
-        # n2 = 0
-
-        # seed = copy.deepcopy(self.seed)
-        # reps = copy.deepcopy(self.reps)
-
-        # for i in range(len(gates)):
-            
-        #     # compute number of circuits that have this number of gates starting with a specific gate
-        #     # print(str(len(seed) - 1) + "! / " + str(len(seed) - 1 - (n1 - 1 - i)) + "!")
-        #     # circ_num = m.factorial(len(seed) - 1) / m.factorial(len(seed) - (len(gates) - i))
-        #     n = sum(reps)
-
-        #     gate_index = seed.index(gates[i])
-
-        #     # do some math (honestly surprised this works)
-        #     nfac = m.factorial(n - 1)
-        #     for j in range(gate_index, len(seed)):
-        #         nfac /= m.factorial(reps[j])
-        #     var_fac_sum = 0
-        #     for j in range(0, gate_index):
-        #         var_temp = 1
-        #         for k in range(0, gate_index):
-        #             var_temp /= m.factorial(reps[k]) if k != j else m.factorial(reps[k] - 1)
-        #         var_fac_sum += var_temp
-
-        #     circ_num = nfac * var_fac_sum
-
-        #     # print(round(circ_num))
-
-        #     # add to total index
-        #     n2 += round(circ_num)
-
-        #     # decrement reps
-        #     if reps[gate_index] > 1:
-        #         reps[gate_index] -= 1
-        #     else:
-        #         reps.pop(gate_index)
-        #         seed.pop(gate_index)
-
-        #     n -= 1
-
         # get index of circuit
         n1, n2 = self.circuit_indexes[tuple(gates)]
 
@@ -177,70 +229,11 @@ class CircuitCollection:
         # print run time
         print("--- Build: %s seconds ---" % (time.time() - build_start_time))
 
-    # transpile circuits with n processes and transpile_args
-    # trials specifies how many times to try transpile before choosing the best result (based on circuit depth)
-    def transpile_circuits(self, n, trials, **transpile_args):
-        trans_start_time = time.time()
-        if not self.built_circuits:
-            print("transpile_circuits: No Circuits Built!")
-            return
-        
-        self.q_transpiled = []
-
-        transpiled_temp = []
-
-        # compute first set of transpiled circuits
-        for i in range(len(self.q_circuits)):
-            self.q_transpiled.append(
-                parallel_map(transpile, self.q_circuits[i], num_processes = n, task_kwargs = transpile_args)
-            )
-
-        print("--- Transpile (Trial " + str(1) + "): %s seconds ---" % (time.time() - trans_start_time))
 
 
-        # compute remaining trials and compare them to the first set, replacing them if they are better
-        for i in range(trials - 1):
-            for j in range(len(self.q_circuits)):
+############ DATASET ############
+# defines a dataset of circuits, with dynamic loading and saving for large datasets
+# will randomize order of circuits, while ensuring that each circuit is in the same pickle file as its children
+# inherits from CircuitCollectionTemplate to ensure that it is backwards compatible
 
-                # Transpile circuits
-                transpiled_temp.append(
-                    parallel_map(transpile, self.q_circuits[j], num_processes = n, task_kwargs = transpile_args)
-                )
-
-                # Compare circuits
-                for k in range(len(self.q_circuits[j])):
-                    if (transpiled_temp[j][k].depth() < self.q_transpiled[j][k].depth()):
-                        self.q_transpiled[j][k] = transpiled_temp[j][k]
-
-             # print run time
-            print("--- Transpile (Trial " + str(i + 2) + "): %s seconds ---" % (time.time() - trans_start_time))
-            transpiled_temp = []
-
-        # print run time
-        print("--- Transpile (" + str(trials) + "): %s seconds ---" % (time.time() - trans_start_time))
-
-    # convert all circuits to image based representation
-    def convert_to_images(self):
-        # time how long it takes to convert all circuits
-        convert_start_time = time.time()
-
-        if not self.built_circuits:
-            print("convert_to_images: No Circuits Built!")
-            return
-
-        # FIXME: why did I think this was good ? lol
-        for i in range(len(self.circuits)):
-            # get index of all circuits
-            indecies = map(self.gates_to_index, self.circuits[i])
-
-            self.images.append(list(map(self.convert_to_image, indecies)))
-
-        # print run time
-        print("--- Convert: %s seconds ---" % (time.time() - convert_start_time))
-
-    def num_circuits(self):
-        sum = 0
-        for i in range(len(self.circuits)):
-            sum += len(self.circuits[i])
-        
-        return sum
+# class CircuitDataset(CircuitCollectionTemplate):
